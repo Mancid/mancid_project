bc082cd5b5f930908f9d81c64aabc049dad76c4e
Nico34000 <prodhomme.nicolas@hotmail.fr>
2021-05-25 11:39:34 +0200

    add wsgi , add config sql for heroku

--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ import os
 from sqla_wrapper import SQLAlchemy
 from flask_login import UserMixin
 
-DB = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///db.sqlite"))
+DB = SQLAlchemy(os.environ.get('DATABASE_URL').replace("://", "ql://", 1))
 # this connects to a database either on Heroku or on localhost
 
 
