1c5e9e4b8395df86ceeccb6a730ea830c2d31bf8
Nico34000 <prodhomme.nicolas@hotmail.fr>
2021-05-28 14:33:10 +0200

    workflow sphinx-doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,9 @@
 import os
 import sys
 import sphinx_rtd_theme
+
+sys.path.insert(0, os.path.abspath('../../backend'))
 import backend
-sys.path.insert(0, os.path.abspath('../../backend')
 
 
 # -- Project information -----------------------------------------------------
