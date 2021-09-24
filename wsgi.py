0fa26afb4181a18dc0b26608be8c09167a5d9aab
DamienPlantin <damien2809@gmail.com>
2021-09-24 19:44:30 +0200

    update project

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 import backend
+import os
 
 
 app = backend.create_app('production')
 
 if __name__ == "__main__":
-  # Bind to PORT if defined, otherwise default to 5000.
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
