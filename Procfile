a758459739cd2cc869e924c7281be810d5a3d410
DamienPlantin <damien2809@gmail.com>
2021-10-16 02:12:38 +0200

    modif launch app

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 build:
-    web: sh Script.sh
+    web: gunicorn --bind 0.0.0.0:${PORT} wsgi:app
--- Script.sh
+++ Script.sh
@@ -1,3 +1,2 @@
 export FLASK_APP="backend"
-# gunicorn --bind 0.0.0.0:5000 wsgi:app
 gunicorn --bind 0.0.0.0:${PORT} wsgi:app
